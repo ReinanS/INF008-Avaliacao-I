b26b8f01aa2224a014c21473ab3ddad383946f20
reinanS <reinandesouza01@gmail.com>
2021-03-16 21:26:17 -0300

    feat: adiciona metodos valida atributos

--- src/RGB.java
+++ src/RGB.java
@@ -4,10 +4,33 @@ public class RGB {
     private double blue;
 
     public RGB(double _red, double _green, double _blue){
-        this.red = _red;
-        this.green = _green;
-        this.blue = _blue;
+        this.red = validaRed(_red);
+        this.green = validaGreen(_green);
+        this.blue = validaBlue(_blue);
     };
+
+    public double validaRed(double _red) {
+        if(_red < 0)  
+            return 0;
+         else if(_red > 255) return 255;
+         else return _red;
+    }
+
+
+    public double validaGreen(double _green) {
+        if(_green < 0)  
+            return 0;
+         else if(_green > 255) return 255;
+         else return _green;
+    }
+
+    public double validaBlue(double _blue) {
+        if(_blue < 0)  
+            return 0;
+         else if(_blue > 255) return 255;
+         else return _blue;
+    }
+
 
     public int getLuminosidade() {
         double luminosidade = ((this.red*0.3) + (this.green*0.59) + (this.blue*0.11)) / 255;
