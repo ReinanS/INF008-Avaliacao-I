201574aed5446936e82629420a2d671f382c5ded
Victor Cerqueira <34761298+cerqueirav@users.noreply.github.com>
2021-03-18 02:01:20 -0300

    Update RGB.java
    Adição de função para converter decimal para Hexa e posterior criação da cor em Hexadecimal

--- src/RGB.java
+++ src/RGB.java
@@ -1,36 +1,55 @@
 public class RGB {
-    private double red;
-    private double green;
-    private double blue;
+    private int red;
+    private int green;
+    private int blue;
 
-    public RGB(double _red, double _green, double _blue){
+    public RGB(int _red, int _green, int _blue){
         this.red = validaRed(_red);
         this.green = validaGreen(_green);
         this.blue = validaBlue(_blue);
-    };
+    }
 
-    public double validaRed(double _red) {
+    public int validaRed(int _red) {
         if(_red < 0)  
             return 0;
          else if(_red > 255) return 255;
          else return _red;
     }
 
-
-    public double validaGreen(double _green) {
+    public int validaGreen(int _green) {
         if(_green < 0)  
             return 0;
          else if(_green > 255) return 255;
          else return _green;
     }
 
-    public double validaBlue(double _blue) {
+    public int validaBlue(int _blue) {
         if(_blue < 0)  
             return 0;
          else if(_blue > 255) return 255;
          else return _blue;
     }
 
+    public String decimalToHexa(int decimal){    
+        int rem;  
+        String hex="";   
+        char hexchars[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};  
+        while(decimal>0)  
+        {  
+          rem=decimal%16;   
+          hex=hexchars[rem]+hex;   
+          decimal=decimal/16;  
+        }  
+       return hex;  
+    }
+
+    public String getColorHex(){
+        String sRed = decimalToHexa(this.red);
+        String sGreen = decimalToHexa(this.green);
+        String sBlue = decimalToHexa(this.blue);
+         
+        return (sRed + sGreen + sBlue); 
+    }
 
     public int getLuminosidade() {
         double luminosidade = ((this.red*0.3) + (this.green*0.59) + (this.blue*0.11)) / 255;
