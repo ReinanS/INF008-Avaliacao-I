f28d2a03a845ad2833524151a5f3fb6e5940a5ef
reinanS <reinandesouza01@gmail.com>
2021-03-21 09:50:06 -0300

    ref: renomeia testeRGB para main.dart

---
+++ src/main.java
@@ -0,0 +1,11 @@
+public class main {
+    public static void main(String[] args) {
+        RGB color = new RGB(148, 44, 211);
+
+        RGB color2 = new RGB(5, 79, 119);
+
+        System.out.println(color2.getColorHex());
+
+       
+    }
+}
