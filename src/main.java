6762b4b2aee511e5a384b7f6dcc4e1dbd6207f93
Victor Cerqueira <34761298+cerqueirav@users.noreply.github.com>
2021-03-22 00:26:47 -0300

    Inclusão de teste unitário
    Incluindo teste unitário

--- src/main.java
+++ src/main.java
@@ -1,11 +1,18 @@
 public class main {
     public static void main(String[] args) {
-        RGB color = new RGB(148, 44, 211);
-
+        
         RGB color2 = new RGB(5, 79, 119);
 
-        System.out.println(color2.getColorHex());
-
+        System.out.println(color2.getLuminosidade()); 
+        
+        // Metodo Clarear
+        RGB colorTeste3 = new RGB(37, 150, 190); 
        
+        // Input: #2596BE
+        colorTeste3.clarear(0.1);
+         
+        // Output: #2187AB
+        System.out.println(colorTeste3.getColorHex());
+
     }
 }
