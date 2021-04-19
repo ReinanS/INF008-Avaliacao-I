8e620e5b86b9cd18238d1d9b463388bef8718ad1
Victor Cerqueira <34761298+cerqueirav@users.noreply.github.com>
2021-04-19 04:43:54 -0300

    Update TesteRGB.java
    Atualização dos testes unitários

--- src/TesteRGB.java
+++ src/TesteRGB.java
@@ -9,5 +9,19 @@ public class TesteRGB {
 
         RGB color3 = new RGB(37, 150, 190); 
         color3.TesteConverteEmCinza();
+
+        Imagem img = new Imagem(3, 5);
+        RGB color4 = new RGB (40, 253, 251);
+
+        System.out.println("Antes + ");
+        img.exibirImagem(); 
+
+        // Metodos sobrecarregados
+        img.modificarPixelDaImagem(0, 2, color4);
+
+        //img.modificarPixelDaImagem(0, 2, 42, 155, 234);
+
+        System.out.println("Depois + ");
+        img.exibirImagem(); 
     }
 }
